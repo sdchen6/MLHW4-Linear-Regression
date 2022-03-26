1c53cd582f2a55f3d6daf68924bbd0446162a2ce
sdchen6 <92230897+sdchen6@users.noreply.github.com>
2022-03-25 23:28:15 -0500

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+from .metrics import mean_squared_error
+from .polynomial_regression import PolynomialRegression
+from .generate_regression_data import generate_regression_data
+from .load_json_data import load_json_data
+
+# Fix for determinism.
+import numpy as np 
+np.random.seed(0)
---
+++ src/metrics.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def mean_squared_error(estimates, targets):
+    """
+    Mean squared error measures the average of the square of the errors (the
+    average squared difference between the estimated values and what is 
+    estimated. The formula is:
+
+    MSE = (1 / n) * \sum_{i=1}^{n} (Y_i - Yhat_i)^2
+
+    Implement this formula here, using numpy and return the computed MSE
+
+    https://en.wikipedia.org/wiki/Mean_squared_error
+
+    Args:
+        estimates(np.ndarray): the estimated values (should be the same shape as targets)
+        targets(np.ndarray): the ground truth values
+
+    Returns:
+        MSE(int): mean squared error calculated by above equation 
+    """
+    sq_sum = 0
+    for i in range(0,len(estimates)):
+        temp = estimates[i] - targets[i]
+        sq_sum += temp ** 2
+    
+    return sq_sum/len(estimates)
+
+    raise NotImplementedError()
